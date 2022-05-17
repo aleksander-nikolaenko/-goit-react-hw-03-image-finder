c53acdc0467db0b5a951b21097f2d598e8d55401
Oleksandr Nikolaienko <aleksander.nikolaenko@gmail.com>
2022-05-17 21:21:29 +0300

    test

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-// import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { Container } from './Container';
 
 export class App extends Component {
   state = {};
 
   render() {
+    Notify.info('Notify works');
     return <Container>fhfdhfgjhfg</Container>;
   }
 }
