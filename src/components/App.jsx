e5c416b330e2e1509db1bff4aca7c77846f22f43
Oleksandr Nikolaienko <aleksander.nikolaenko@gmail.com>
2022-05-17 21:06:12 +0300

    add start files

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { Container } from './Container';
+
+export class App extends Component {
+  state = {};
+
+  render() {
+    return <Container>fhfdhfgjhfg</Container>;
+  }
+}
